097653d92875993480a2f7fb7fd84bc6db0c8129
Carlos.Inhani <carlosinhani>
2020-10-29 19:36:49 -0300

    corousel_p1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
+import logo from '../../assets/logo.svg';
 
-import { Container } from './styles'
+import { Container, Search } from './styles'
 
-const Home = () => <Container>Helloo word!</Container>;
+const Home = () => {
+  const [inputValue, setInpulValue] = useState('');
 
+  return ( <Container>
+      <Search>
+        <img src={logo} alt="Logo do Restaurante" />
+        <TextField
+          label='Pesquisar'
+          outlined
+          //onTrailingIconSelect={() => this.setState({value: ''})}
+         // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+        >
+          <Input
+           value={inputValue}
+           onChange={(e) => setInpulValue(e.target.value)} />
+        </TextField>
+      </Search>
+   </Container>
+   );
+};
 export default Home;
