c1f4937b43a493498fabf2d4b3517cfd09b8ca31
Carlos.Inhani <carlosinhani>
2020-10-30 12:55:36 -0300

    dev_corousel

---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  width: 90px;
+  height: 90px;
+  border-radius: 6px;
+  background-color: ${(props) => props.photo};
+  background-size: cover;
+`;
+
+const ImageCard = ({ photo }) => <Card photo={photo} />;
+
+export default ImageCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
+import Slider from "react-slick";
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-import { Container, Search } from './styles'
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles'
 
 const Home = () => {
   const [inputValue, setInpulValue] = useState('');
 
-  return ( <Container>
-      <Search>
-        <img src={logo} alt="Logo do Restaurante" />
-        <TextField
-          label='Pesquisar'
-          outlined
-          //onTrailingIconSelect={() => this.setState({value: ''})}
-         // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-        >
-          <Input
-           value={inputValue}
-           onChange={(e) => setInpulValue(e.target.value)} />
-        </TextField>
-      </Search>
-   </Container>
-   );
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do Restaurante" />
+          <TextField
+            label='Pesquisar Restaurantes'
+              outlined
+              trailingIcon={<MaterialIcon role="button" icon="search"/>}>
+            <Input value={inputValue}onChange={(e) => setInpulValue(e.target.value)} />
+          </TextField>
+          <CarouselTitle>Perto de Você</CarouselTitle>
+          <Slider { ...settings}>
+            < Card photo={restaurante} />
+            < Card photo={restaurante} />
+            < Card photo={restaurante} />
+            < Card photo={restaurante} />
+            < Card photo={restaurante} />
+          </Slider>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
 };
 export default Home;
