ee066f287ec46256b0c812617be4ee4429df94ad
Carlos.Inhani <carlos.inhani@gmail.com>
2020-10-31 17:37:51 -0300

    add_redux

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,6 +10,7 @@ import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle } from './sty
 
 const Home = () => {
   const [inputValue, setInpulValue] = useState('');
+  const [query, setQuery] = useState(null);
   const [modalOpened, setModalOpened] = useState(true);
 
   const settings = {
@@ -21,6 +22,12 @@ const Home = () => {
     adaptiveHeight: true,
   };
 
+  function handleKeyPress(e) {
+    if (e.Key === 'Enter') {
+      setQuery(inputValue);
+    }
+  }
+
   return (
     <Wrapper>
       <Container>
@@ -30,7 +37,11 @@ const Home = () => {
             label='Pesquisar Restaurantes'
               outlined
               trailingIcon={<MaterialIcon role="button" icon="search"/>}>
-            <Input value={inputValue}onChange={(e) => setInpulValue(e.target.value)} />
+              <Input
+                value={inputValue}
+                onKeyPress={handleKeyPress}
+                onChange={(e) => setInpulValue(e.target.value)}
+              />
           </TextField>
           <CarouselTitle>Perto de Você</CarouselTitle>
           <Carousel { ...settings}>
@@ -45,7 +56,7 @@ const Home = () => {
         </Search>
         <RestaurantCard />
       </Container>
-      <Map />
+      <Map query={query} />
       {/* <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} /> */}
     </Wrapper>
   );
